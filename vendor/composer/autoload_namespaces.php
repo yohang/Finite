773024f918c2fdc8b5fc8b1b6f4ddd06162c20e0
yohang <yohan@giarel.li>
2012-10-06 15:33:50 +0200

    Added QA stuff

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,5 +6,7 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Symfony\\Component\\Yaml' => $vendorDir . '/symfony/yaml/',
+    'Symfony\\Component\\EventDispatcher' => $vendorDir . '/symfony/event-dispatcher/',
     'Finite' => $baseDir . '/src/',
 );
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -18,6 +18,10 @@ class ComposerAutoloaderInit
         $vendorDir = dirname(__DIR__);
         $baseDir = dirname($vendorDir);
 
+        $includePaths = require __DIR__ . '/include_paths.php';
+        array_push($includePaths, get_include_path());
+        set_include_path(join(PATH_SEPARATOR, $includePaths));
+
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->add($namespace, $path);
