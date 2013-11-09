94301140bc69d423ea5bdc41b7a6104c7f92b027
Daniel Pozzi <bonndan76@googlemail.com>
2013-11-09 18:07:59 +0100

    visualisation exception

---
+++ src/Finite/Visualisation/Exception.php
@@ -0,0 +1,12 @@
+<?php
+namespace Finite\Visualisation;
+
+/**
+ * Visualisation exception.
+ *
+ * @author Daniel Pozzi <bonndan76@googlemail.com>
+ */
+class Exception extends \Finite\Exception\Exception
+{
+    
+}
