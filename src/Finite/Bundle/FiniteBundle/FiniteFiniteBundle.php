79a83c576dcc2e8b10a05ddef2757cde9478abe0
Alexandre <alexandre.bacco@gmail.com>
2014-04-30 14:33:54 +0800

    Use container services in loader configs

--- src/Finite/Bundle/FiniteBundle/FiniteFiniteBundle.php
+++ src/Finite/Bundle/FiniteBundle/FiniteFiniteBundle.php
@@ -2,8 +2,17 @@
 
 namespace Finite\Bundle\FiniteBundle;
 
+use Finite\Bundle\FiniteBundle\DependencyInjection\Compiler\ContainerCallbackPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class FiniteFiniteBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new ContainerCallbackPass());
+    }
 }
