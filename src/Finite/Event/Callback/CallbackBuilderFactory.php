c72bbeafabfc6a6fd854ae44162a1648e6872dcf
yohang <yohan@giarel.li>
2014-05-14 11:28:58 +0200

    Addded a CallbackBuilder factory to make it overridable by DI

---
+++ src/Finite/Event/Callback/CallbackBuilderFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Finite\Event\Callback;
+
+use Finite\StateMachine\StateMachineInterface;
+
+/**
+ * Concrete implementation of CallbackBuilder factory
+ *
+ * @author Yohan Giarelli <yohan@frequence-web.fr>
+ */
+class CallbackBuilderFactory implements CallbackBuilderFactoryInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function createBuilder(StateMachineInterface $stateMachine)
+    {
+        return CallbackBuilder::create($stateMachine);
+    }
+}
---
+++ src/Finite/Event/Callback/CallbackBuilderFactoryInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Finite\Event\Callback;
+
+use Finite\StateMachine\StateMachineInterface;
+
+/**
+ * Base interface for CallbackBuilder factories
+ *
+ * @author Yohan Giarelli <yohan@frequence-web.fr>
+ */
+interface CallbackBuilderFactoryInterface
+{
+    /**
+     * @param StateMachineInterface $stateMachine
+     *
+     * @return mixed
+     */
+    public function createBuilder(StateMachineInterface $stateMachine);
+}
