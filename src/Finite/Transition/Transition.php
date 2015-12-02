2f3e6f966266dd7900a2f09a39abf49bbf8f4a57
yohang <yohan@giarel.li>
2015-12-02 15:51:57 +0100

    Fixed an incompatibility with PHP < 5.6

--- src/Finite/Transition/Transition.php
+++ src/Finite/Transition/Transition.php
@@ -165,11 +165,14 @@ class Transition implements PropertiesAwareTransitionInterface
     public function getProperties()
     {
         $missingOptions = $this->propertiesOptionsResolver->getMissingOptions();
+        $options        = [];
+
+        if (count($missingOptions) > 0) {
+            $options = array_combine($missingOptions, array_fill(0, count($missingOptions), null));
+        }
 
         return array_diff_key(
-            $this->propertiesOptionsResolver->resolve(
-                array_combine($missingOptions, array_fill(0, count($missingOptions), null))
-            ),
+            $this->propertiesOptionsResolver->resolve($options),
             array_combine($missingOptions, $missingOptions)
         );
     }
