4d948905d8b9f973b338a225a6d4f87dbd3fd597
Luis Henrique Mulinari <luis.mulinari@gmail.com>
2016-01-30 16:07:11 +0000

    Created ContextInterface

---
+++ src/Finite/ContextInterface.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Finite;
+
+use Finite\StateMachine\StateMachineInterface;
+
+/**
+ * Interface that all class that have properties must implements
+ *
+ * @author Luis Henrique Mulinari <luis.mulinari@gmail.com>
+ */
+interface ContextInterface
+{
+    /**
+     * @param object $object
+     * @param string $graph
+     *
+     * @return string
+     */
+    public function getState($object, $graph = 'default');
+
+    /**
+     * @param object $object
+     * @param string $graph
+     * @param bool   $asObject
+     *
+     * @return array<string>
+     */
+    public function getTransitions($object, $graph = 'default', $asObject = false);
+
+    /**
+     * @param object $object
+     * @param string $graph
+     *
+     * @return array<string>
+     */
+    public function getProperties($object, $graph = 'default');
+
+    /**
+     * @param object $object
+     * @param string $property
+     * @param string $graph
+     *
+     * @return bool
+     */
+    public function hasProperty($object, $property, $graph = 'default');
+
+    /**
+     * @param object $object
+     * @param string $graph
+     *
+     * @return StateMachineInterface
+     */
+    public function getStateMachine($object, $graph = 'default');
+}
